98f96078126728a8c455f2f71e635deccdbcdc1d
BISHAL DATTA <dattabishal001@gmail.com>
2024-07-31 18:24:48 +0530

    feat: implement navbar for improved navigation and user experience
    - Introduced a new responsive navbar with enhanced styling
    - Added dropdown menus and hover effects for better interactivity
    - Improved accessibility features and cross-browser support
    
    This initial implementation enhances the overall navigation experience for users.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
@@ -13,7 +14,7 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      darkMode: "class",
+      
     },
   },
   plugins: [],
