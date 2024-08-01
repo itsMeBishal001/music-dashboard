898341700dd26dfef9c9910e9faf51defdad6051
BISHAL DATTA <dattabishal001@gmail.com>
2024-08-01 17:23:04 +0530

    feat: add Featured Card section to music web app
    - Implemented a new Featured Card section to highlight key music albums or artists
    - Designed cards to display album/artist information with responsive styling
    - Integrated dynamic content loading for showcasing featured items
    - Enhanced user engagement by providing visual appeal and easy navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCources from "@/components/FeaturedCources";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <h1 className="text-center text-3xl">music web by Bishal ...</h1>
       <HeroSection/>
+      <FeaturedCources/>
     </main>
   );
 }
