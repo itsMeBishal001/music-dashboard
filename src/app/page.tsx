c6c74bde8e8c523f768013806966af15d7475bdd
BISHAL DATTA <dattabishal001@gmail.com>
2024-08-02 18:03:06 +0530

    feat: add routing for Courses and Contact Us pages, footer, hover effects, background beam, and animated tooltip
    - Implement routing for Courses and Contact Us pages
    - Add footer component to improve site navigation
    - Apply hover effects on cards for better interactivity
    - Introduce background beam for visual appeal
    - Add animated tooltip for enhanced user guidance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import HeroSection from "@/components/HeroSection";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import   MusicSchoolTestimonials  from "@/components/TestimonialCards";
 import  UpcomingWebinars  from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -13,6 +15,8 @@ export default function Home() {
       <WhyChooseUs/>
       <MusicSchoolTestimonials/>
       <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
     </main>
   );
 }
